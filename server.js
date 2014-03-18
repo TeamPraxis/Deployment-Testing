cf492b68d71220dff4a849aa2af896d2cd188da7
Justin Zaun <jzaun@teampraxis.com>
2014-03-17 15:57:27 -1000

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+console.log('start');
+http.createServer(function (req, res) {
+  console.log('created');
+  setTimeout(function() {
+    console.log('sent');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+  }, 1000);
+}).listen(3000);
