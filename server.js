c1974866135c8189805c8a78e829a8da18f9e4a6
brobra <bobra@teampraxis.com>
2014-03-24 11:38:58 -1000

    Update server.js

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ http.createServer(function (req, res) {
     console.log('sent');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-  }, 1000*40);
+  }, 1000);
 }).listen(80);
