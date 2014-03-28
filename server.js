2d7cd342f39ca9c1175a22639fef054b94e61802
brobra <bobra@teampraxis.com>
2014-03-28 13:59:40 -1000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ console.log('start');
 http.createServer(function (req, res) {
   console.log('created');
   setTimeout(function() {
-
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
   }, 1000);
