fc1707e7cc3723fdee3548ca9546ab8d56897718
brobra <bobra@teampraxis.com>
2014-03-28 13:42:40 -1000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ console.log('start');
 http.createServer(function (req, res) {
   console.log('created');
   setTimeout(function() {
-    for(int i = 0; i < 1000000); i++){
+    for(var i = 0; i < 1000000); i++){
       console.log("I just walked "+i+" miles");
     }
     console.log('sent');
